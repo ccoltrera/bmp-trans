11b5c699a034de47b88bffb76c07daa03dab94c8
Colin Coltrera <c.coltrera@gmail.com>
2015-07-10 17:55:31 -0700

    Add ability to read bmp files, test for compatibility, apply one of 4 specified transformations, and write to new files.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 "use strict";
 
-var algorithms = require("./lib/bmp-trans");
+var bmpTrans = require("./lib/bmp-trans");
+
+bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "red");
+bmpTrans.transform("./bmps/palette-bitmap.bmp", "red");
+
+bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "green");
+bmpTrans.transform("./bmps/palette-bitmap.bmp", "green");
+
+bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "blue");
+bmpTrans.transform("./bmps/palette-bitmap.bmp", "blue");
+
+bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "invert");
+bmpTrans.transform("./bmps/palette-bitmap.bmp", "invert");
--- test/bmp-trans-test.js
+++ test/bmp-trans-test.js
@@ -3,10 +3,3 @@
 var expect = require("chai").expect;
 var bmpTrans = require("../lib/bmp-trans");
 
-describe("fileReader", function() {
-  it("should read a file, specified by path, and return a Buffer() object", function() {
-    var buf = bmpTrans.fileReader("../bmps/non-palette-bitmap.bmp");
-    expect(typeof buf).to.eql("object");
-    expect(buf).to.be.an.instanceof(Buffer);
-  });
-});
