aba36f7631d4b003d4ce8c01d12399185623d98a
Colin Coltrera <c.coltrera@gmail.com>
2015-07-13 10:34:07 -0700

    Add .jshintrc file, tests for checkBMP.

--- index.js
+++ index.js
@@ -2,14 +2,4 @@
 
 var bmpTrans = require("./lib/bmp-trans");
 
-// bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "red");
-// bmpTrans.transform("./bmps/palette-bitmap.bmp", "red");
-
-// bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "green");
-// bmpTrans.transform("./bmps/palette-bitmap.bmp", "green");
-
-// bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "blue");
-// bmpTrans.transform("./bmps/palette-bitmap.bmp", "blue");
-
-bmpTrans.transform("./bmps/non-palette-bitmap.bmp", process.argv[2]);
-bmpTrans.transform("./bmps/palette-bitmap.bmp", process.argv[2]);
+bmpTrans.transform(process.argv[2], process.argv[3]);
--- test/bmp-trans-test.js
+++ test/bmp-trans-test.js
@@ -2,13 +2,33 @@
 
 var expect = require("chai").expect;
 var bmpTrans = require("../lib/bmp-trans");
+var fs = require("fs");
 
 describe("bmpTrans", function() {
-  beforeEach(function() {
-
-  });
-  describe("invert", function() {
-
+  describe("checkBMP", function() {
+    it("should analyze a buffer, return null if that file not a Windows Bitmap file", function() {
+      fs.readFile("./bmps/empty-file.bmp", function(error, data) {
+        expect(bmpTrans.checkBMP(data)).to.deep.equal(null);
+      });
+    });
+    it("should return an object. If no palette data, return .start as 54, .end as the length of the data, and .colorSpec as 3", function() {
+      fs.readFile("./bmps/non-palette-bitmap.bmp", function(error, data) {
+        expect(bmpTrans.checkBMP(data)).to.deep.equal({
+        start: 54,
+        end: 30054,
+        colorSpec: 3
+       });
+      });
+    });
+    it("should return an object. If palette data, return .start as 54, .end as img data offset, and .colorSpec as 4", function() {
+      fs.readFile("./bmps/palette-bitmap.bmp", function(error, data) {
+        expect(bmpTrans.checkBMP(data)).to.deep.equal({
+        start: 54,
+        end: 1078,
+        colorSpec: 4
+       });
+      });
+    });
   });
 });
 
