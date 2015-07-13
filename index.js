4d00a245dac5d47a6ba4949c6f3b28f22dc67c80
Colin Coltrera <c.coltrera@gmail.com>
2015-07-13 09:27:31 -0700

    Add CLI

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 var bmpTrans = require("./lib/bmp-trans");
 
-bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "red");
-bmpTrans.transform("./bmps/palette-bitmap.bmp", "red");
+// bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "red");
+// bmpTrans.transform("./bmps/palette-bitmap.bmp", "red");
 
-bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "green");
-bmpTrans.transform("./bmps/palette-bitmap.bmp", "green");
+// bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "green");
+// bmpTrans.transform("./bmps/palette-bitmap.bmp", "green");
 
-bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "blue");
-bmpTrans.transform("./bmps/palette-bitmap.bmp", "blue");
+// bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "blue");
+// bmpTrans.transform("./bmps/palette-bitmap.bmp", "blue");
 
-bmpTrans.transform("./bmps/non-palette-bitmap.bmp", "invert");
-bmpTrans.transform("./bmps/palette-bitmap.bmp", "invert");
+bmpTrans.transform("./bmps/non-palette-bitmap.bmp", process.argv[2]);
+bmpTrans.transform("./bmps/palette-bitmap.bmp", process.argv[2]);
--- test/bmp-trans-test.js
+++ test/bmp-trans-test.js
@@ -3,3 +3,12 @@
 var expect = require("chai").expect;
 var bmpTrans = require("../lib/bmp-trans");
 
+describe("bmpTrans", function() {
+  beforeEach(function() {
+
+  });
+  describe("invert", function() {
+
+  });
+});
+
